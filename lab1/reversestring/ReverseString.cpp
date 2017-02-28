6b02924d1f82a414569e684fc5dfa8f785983eb9
Jakub Adamczyk <adamczyk777@gmail.com>
2017-02-28 12:27:42 +0100

    add recursive logic to reverse string

--- lab1/reversestring/ReverseString.cpp
+++ lab1/reversestring/ReverseString.cpp
@@ -2,15 +2,19 @@
 // Created by adamjaku on 28.02.17.
 //
 
-
-#include <iostream>
 #include "ReverseString.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 string ReverseString(string str) {
+    if (str.length() <= 1) {
 
+        return str;
+    }
+    else {
 
-
-    return NULL;
+        return ReverseString(str);
+    }
 }

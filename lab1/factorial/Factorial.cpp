c980ff1443f66f90c81cb67da077d6ce95f749a0
Krzysztof Kosecki <junnex.tay@gmail.com>
2017-02-28 11:56:20 +0100

    Factorial, 14 passed

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,6 +4,19 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-
-  return 0;
+  if(value == 0) return 1;
+  int result;
+  if(value > 0) {
+    result = 1;
+    for (int i = 2; i <= value; i++) {
+      result *= i;
+    }
+  }
+  else { // ujemne
+    result = -1;
+    for (int i = -2; i*i <= value*value; i--) {
+      result *= i;
+    }
+  }
+  return result;
 }

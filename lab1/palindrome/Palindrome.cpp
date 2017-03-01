ca9c35d7c814199bdef52cb4f42678d20295cee6
tay0108 <kjkosecki@gmail.com>
2017-03-01 22:39:08 +0100

    Palindrome done.

--- lab1/palindrome/Palindrome.cpp
+++ lab1/palindrome/Palindrome.cpp
@@ -7,8 +7,10 @@ bool is_palindrome(std::string str) {
     int length = str.length();
     int i = 0;
     int j = length - 1;
-    while(i < length / 2 && j > length / 2) {
+    while(i <= length / 2 && j >= length / 2) {
         if(str[i] != str[j]) return false;
+        ++i;
+        j--;
     }
     return true;
 }

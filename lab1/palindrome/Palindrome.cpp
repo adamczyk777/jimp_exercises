0e3830bebea84b9a7e1c7a92093381f9861df19d
tay0108 <kjkosecki@gmail.com>
2017-03-01 22:28:47 +0100

    Resolved string problem

--- lab1/palindrome/Palindrome.cpp
+++ lab1/palindrome/Palindrome.cpp
@@ -1,5 +1,14 @@
 //
 // Created by adamjaku on 28.02.17.
 //
-
 #include "Palindrome.h"
+
+bool is_palindrome(std::string str) {
+    int length = str.length();
+    int i = 0;
+    int j = length - 1;
+    while(i < length / 2 && j > length / 2) {
+        if(str[i] != str[j]) return false;
+    }
+    return true;
+}
--- lab1/palindrome/main.cpp
+++ lab1/palindrome/main.cpp
@@ -2,3 +2,9 @@
 // Created by adamjaku on 28.02.17.
 //
 
+#include "Palindrome.h"
+
+int main() {
+    is_palindrome("asdfdsa");
+    return 0;
+}

4eab34809010e0de1e99822dacc82b380b05cdf8
Jakub Adamczyk <adamczyk777@gmail.com>
2017-03-02 15:27:26 +0100

    initialize MultiplicationTable

--- lab1/multiplicationtable/MultiplicationTable.cpp
+++ lab1/multiplicationtable/MultiplicationTable.cpp
@@ -3,3 +3,7 @@
 //
 
 #include "MultiplicationTable.h"
+
+void MultiplicationTable(int tab[][10]){
+    // TODO: main logci for this function ~Adamczyk
+}
--- lab1/multiplicationtable/MultiplicationTable.h
+++ lab1/multiplicationtable/MultiplicationTable.h
@@ -5,4 +5,8 @@
 #ifndef JIMP_EXERCISES_MULTIPLICATIONTABLE_H
 #define JIMP_EXERCISES_MULTIPLICATIONTABLE_H
 
+#include <iostream>
+
+void MultiplicationTable(int tab[][10]);
+
 #endif //JIMP_EXERCISES_MULTIPLICATIONTABLE_H

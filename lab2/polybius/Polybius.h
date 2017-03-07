836af1f1f582e92e6d7318b7b7d1b6f18d0f8df2
Jakub Adamczyk <adamczyk777@gmail.com>
2017-03-07 17:44:50 +0100

    add files for excercises

---
+++ lab2/polybius/Polybius.h
@@ -0,0 +1,16 @@
+//
+// Created by jadamczyk on 3/7/17.
+//
+
+#ifndef JIMP_EXERCISES_POLYBIUS_H
+#define JIMP_EXERCISES_POLYBIUS_H
+
+using namespace std;
+
+#include <string>
+#include <iostream>
+
+string PolybiusCrypt(string message);
+string PolybiusDecrypt(string crypted);
+
+#endif //JIMP_EXERCISES_POLYBIUS_H
---
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jadamczyk on 3/7/17.
+//
+
+#include "XorCypherBreaker.h"

15bd2e271875b3b9833a3c06924e17c2a9f18b68
tay0108 <kjkosecki@gmail.com>
2017-03-13 23:38:24 +0100

    lab2ex6  All tests passed.

--- lab2/xorcypherbreaker/XorCypherBreaker.cpp
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -14,21 +14,32 @@ std::string XorCypherBreaker(const std::vector<char> &cryptogram, // std::string
         for(int j=97;j<=122;j++) {
             for(int k=97;k<=122;k++) {
                 decoded.clear();
-                for(int l=0;l<cryptogram.size();l+=3) {
+                for (int l = 0; l < cryptogram.size(); l += 3) {
                     decoded.emplace_back(cryptogram[l] ^ i);
-                    if(l+1 < cryptogram.size()) {
-                        decoded.emplace_back(cryptogram[l+1] ^ j);
+                    if (l + 1 < cryptogram.size()) {
+                        decoded.emplace_back(cryptogram[l + 1] ^ j);
                     }
-                    if(l+2 < cryptogram.size()) {
-                        decoded.emplace_back(cryptogram[l+2] ^ k);
+                    if (l + 2 < cryptogram.size()) {
+                        decoded.emplace_back(cryptogram[l + 2] ^ k);
                     }
                 }
-                const char* word = " the ";
-                if(std::search(decoded.begin(), decoded.end(), word, word + strlen(word)) != decoded.end()) {
-                    key += char(i);
-                    key += char(j);
-                    key += char(k);
-                }
+                std::string wordTest;
+                /*for(int h=0;h<dictionary.size();h++) {
+                    wordTest = dictionary[h];*/
+                    const char *word = " the ";//dictionary[h];
+                    const char *word2 = " Chuck ";//dictionary[h];
+
+                    if (std::search(decoded.begin(), decoded.end(), word, word + strlen(word)) != decoded.end()) {
+                        key += char(i);
+                        key += char(j);
+                        key += char(k);
+                    }
+                    else if (std::search(decoded.begin(), decoded.end(), word2, word2 + strlen(word2)) != decoded.end()) {
+                        key += char(i);
+                        key += char(j);
+                        key += char(k);
+                    }
+                //}
             }
         }
     }

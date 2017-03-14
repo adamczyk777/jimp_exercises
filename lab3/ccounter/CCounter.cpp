ac62bb94de4c867ea9d703c167945b0eb8b6e71d
Jakub Adamczyk <adamczyk777@gmail.com>
2017-03-14 12:31:15 +0100

    add working files to repository CCounter

--- lab3/ccounter/CCounter.cpp
+++ lab3/ccounter/CCounter.cpp
@@ -4,12 +4,30 @@
 
 #include "CCounter.h"
 
-void Inc(std::string key, std::unique_ptr<Counter>* counter){
-
+ccounter::unique_ptr<ccounter::Counter> ccounter::Init(){
+    ccounter::unique_ptr<ccounter::Counter> object = std::make_unique<ccounter::Counter>();
+    return object;
 }
-int Counts(const std::unique_ptr<Counter> &counter, std::string key){
 
+void ccounter::Inc(std::string key, std::unique_ptr<Counter>* counter){
+    bool exists = false;
+    for (auto obj : counter->get()->objects){
+        if (obj.first == key) {
+            obj.second++;
+            exists = true;
+        }
+    }
+    if (!exists){
+        counter->get()->objects.emplace(key, 1);
+    }
 }
-void SetCountsTo(std::string key, int value, std::unique_ptr<Counter> *counter){
+int ccounter::Counts(const std::unique_ptr<Counter> &counter, std::string key){
+    for (auto obj : counter->objects) {
+        if (obj.first == key){
+            return obj.second;
+        }
+    }
+}
+void ccounter::SetCountsTo(std::string key, int value, std::unique_ptr<Counter> *counter){
 
 }
--- lab3/ccounter/CCounter.h
+++ lab3/ccounter/CCounter.h
@@ -4,16 +4,19 @@
 
 #ifndef JIMP_EXERCISES_CCOUNTER_H
 #define JIMP_EXERCISES_CCOUNTER_H
-
 #include <string>
 #include <memory>
 #include <map>
+namespace ccounter{
+    using std::unique_ptr;
+    struct Counter {
+        std::map<std::string, int> objects;
+    };
 
-using namespace std;
+    std::unique_ptr<Counter> Init();
+    void Inc(std::string key, std::unique_ptr<Counter>* counter);
+    int Counts(const std::unique_ptr<Counter> &counter, std::string key);
+    void SetCountsTo(std::string key, int value, std::unique_ptr<Counter> *counter);
 
-std::unique_ptr<Counter> Init();
-void Inc(std::string key, std::unique_ptr<Counter>* counter);
-int Counts(const std::unique_ptr<Counter> &counter, std::string key);
-void SetCountsTo(std::string key, int value, std::unique_ptr<Counter> *counter);
-
+}
 #endif //JIMP_EXERCISES_CCOUNTER_H

d053d50af31627f567e62e3dd248a49e4eb49192
Krzysiek Kosecki <junnex.tay@gmail.com>
2017-03-18 16:13:43 +0100

    Smart Three project setup.

---
+++ lab3/smarttree/SmartTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tay on 3/18/2017.
+//
+
+#include "SmartTree.h"
---
+++ lab3/smarttree/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tay on 3/18/2017.
+//
+
+#include "SmartTree.h"
+
+int main() {
+    return 0;
+}

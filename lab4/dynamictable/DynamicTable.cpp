01de12fcf52a385cc9b9185eccaf77a83e28c101
tay0108 <kjkosecki@gmail.com>
2017-03-21 12:09:53 +0100

    improved SquareClass

--- lab4/dynamictable/DynamicTable.cpp
+++ lab4/dynamictable/DynamicTable.cpp
@@ -7,3 +7,31 @@
 void DTab::resize(int newSize) {
 
 }
+
+DTab::DTab() {
+
+}
+
+DTab::DTab(int initLength) {
+
+}
+
+DTab::~DTab() {
+
+}
+
+void DTab::add(double element) {
+
+}
+
+double DTab::get(int index) {
+    return this->tab[index];
+}
+
+void DTab::set(double element, int index) {
+ this
+}
+
+void DTab::print() {
+
+}

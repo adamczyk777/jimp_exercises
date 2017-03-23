369c39ebc664edd06ad08745d08b560dc1891361
Jakub Adamczyk <adamczyk777@gmail.com>
2017-03-23 15:14:30 +0100

    Add basic constructors getters and setters, still two functions to implement

--- lab4/netsjson/SimpleJson.cpp
+++ lab4/netsjson/SimpleJson.cpp
@@ -3,3 +3,96 @@
 //
 
 #include "SimpleJson.h"
+
+
+nets::JsonValue::~JsonValue() {
+
+}
+
+nets::JsonValue::JsonValue(int jsonInt) : jsonInt(jsonInt) {
+    this->jsonInt = jsonInt;
+}
+
+nets::JsonValue::JsonValue(double jsonDouble) : jsonDouble(jsonDouble) {
+    this->jsonDouble = jsonDouble;
+}
+
+nets::JsonValue::JsonValue(const string &jsonString) : jsonString(jsonString) {
+    this->jsonString = jsonString;
+}
+
+nets::JsonValue::JsonValue(bool jsonBool) : jsonBool(jsonBool) {
+    this->jsonBool=jsonBool;
+}
+
+nets::JsonValue::JsonValue(const vector<JsonValue> &jsonVector) : jsonVector(jsonVector) {
+    this->jsonVector=jsonVector;
+}
+
+nets::JsonValue::JsonValue(const map<string, JsonValue> &jsonMap) : jsonMap(jsonMap) {
+    this->jsonMap=jsonMap;
+}
+
+nets::JsonValue::JsonValue() {}
+
+int nets::JsonValue::getJsonInt() const {
+    return jsonInt;
+}
+
+void nets::JsonValue::setJsonInt(int jsonInt) {
+    JsonValue::jsonInt = jsonInt;
+}
+
+double nets::JsonValue::getJsonDouble() const {
+    return jsonDouble;
+}
+
+void nets::JsonValue::setJsonDouble(double jsonDouble) {
+    JsonValue::jsonDouble = jsonDouble;
+}
+
+const string &nets::JsonValue::getJsonString() const {
+    return jsonString;
+}
+
+void nets::JsonValue::setJsonString(const string &jsonString) {
+    JsonValue::jsonString = jsonString;
+}
+
+bool nets::JsonValue::isJsonBool() const {
+    return jsonBool;
+}
+
+void nets::JsonValue::setJsonBool(bool jsonBool) {
+    JsonValue::jsonBool = jsonBool;
+}
+
+const nets::JsonValue &nets::JsonValue::getJsonValue() const {
+    return *jsonValue;
+}
+
+const vector<nets::JsonValue> &nets::JsonValue::getJsonVector() const {
+    return jsonVector;
+}
+
+void nets::JsonValue::setJsonVector(const vector<JsonValue> &jsonVector) {
+    JsonValue::jsonVector = jsonVector;
+}
+
+void nets::JsonValue::setJsonValue(nets::JsonValue *jsonValue) {
+    JsonValue::jsonValue = jsonValue;
+}
+
+nets::JsonValue::JsonValue(nets::JsonValue *jsonValue) : jsonValue(jsonValue) {
+    this->jsonValue = jsonValue;
+}
+
+string nets::JsonValue::ToString() const {
+
+}
+
+std::experimental::optional<nets::JsonValue> nets::JsonValue::ValueByName(
+        const std::string &name) const {
+
+}
+
--- lab4/netsjson/SimpleJson.h
+++ lab4/netsjson/SimpleJson.h
@@ -5,4 +5,75 @@
 #ifndef JIMP_EXERCISES_SIMPLEJSON_H
 #define JIMP_EXERCISES_SIMPLEJSON_H
 
+#include <experimental/optional>
+#include <string>
+#include <vector>
+#include <map>
+
+using std::string;
+using std::vector;
+using std::map;
+
+namespace nets {
+
+    class JsonValue {
+    public:
+        int getJsonInt() const;
+
+        void setJsonInt(int jsonInt);
+
+        double getJsonDouble() const;
+
+        void setJsonDouble(double jsonDouble);
+
+        const string &getJsonString() const;
+
+        void setJsonString(const string &jsonString);
+
+        bool isJsonBool() const;
+
+        void setJsonBool(bool jsonBool);
+
+        const JsonValue &getJsonValue() const;
+
+        void setJsonValue(JsonValue *jsonValue);
+
+        const vector<JsonValue> &getJsonVector() const;
+
+        void setJsonVector(const vector<JsonValue> &jsonVector);
+
+        JsonValue(const vector<JsonValue> &jsonVector);
+
+        JsonValue(JsonValue *jsonValue);
+
+        JsonValue(bool jsonBool);
+
+        JsonValue(const string &jsonString);
+
+        JsonValue(double jsonDouble);
+
+        JsonValue(int jsonInt);
+
+        JsonValue(const map<string, JsonValue> &jsonMap);
+
+        JsonValue();
+
+        virtual ~JsonValue();
+
+        string ToString() const;
+
+        std::experimental::optional<JsonValue> ValueByName(const std::string &name) const;
+
+    private:
+        int jsonInt;
+        double jsonDouble;
+        string jsonString;
+        bool jsonBool;
+        JsonValue *jsonValue;
+        vector<JsonValue> jsonVector;
+        map<string, JsonValue> jsonMap;
+    };
+
+}
+
 #endif //JIMP_EXERCISES_SIMPLEJSON_H

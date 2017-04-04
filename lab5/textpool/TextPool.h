589a50bae2c606851d7c8590e33c848b4080c383
Krzysiek Kosecki <junnex.tay@gmail.com>
2017-04-04 02:42:07 +0200

    TextPool class methods in header and a couple more things

---
+++ lab5/textpool/TextPool.h
@@ -0,0 +1,43 @@
+//
+// Created by Tay on 4/1/2017.
+//
+
+#ifndef JIMP_EXERCISES_TEXTPOOL_H
+#define JIMP_EXERCISES_TEXTPOOL_H
+
+#include <initializer_list>
+#include <string>
+#include <experimental/string_view>
+#include <set>
+
+namespace pool {
+    class TextPool {
+    public:
+        // Rule of Five (z zakomentowana mozliwoscia kopiowania):
+        // tworzymy obiekt na podstawie istniejacego:
+        // TextPool(const TextPool &obj); // 1. konstruktor kopiujacy (copy constructor) (stary obiekt jest nienatuszony przy przenoszeniu, przekazany przez const)
+        TextPool(TextPool &&obj); // 2. konstruktor przenoszacy (move constructor), rvalue, bierze obiekt tymczasowy (stary obiekt jest niszczony przy przenoszeniu)
+
+        // przypisujemy do zainicjalizowanego obiektu juz zainicjalizowany:
+        // TextPool &operator=(const TextPool &obj); // 3. operator przypisania kopiujacy ()
+        TextPool &operator=(const TextPool &&obj); // 4. operator przypisania przenoszacy ()
+
+        // kasujemy obiekt:
+        ~TextPool(); // 5. destruktor
+
+        // domyslny konstruktor:
+        TextPool();
+        // konstruktor z lista inicjalizacyjna:
+        TextPool(const std::initializer_list<std::pair<const std::experimental::string_view, unsigned int>> &elements) : pool_{elements}; // dunno why error
+        // metoda do insertowania stringa do setu i zwracania go:
+        std::experimental::string_view Intern(const std::string &str);
+        // metoda do zliczania? :
+        size_t StoredStringCount() const; // const po - nie moze modyfikowac elementow skladowych klasy i nie moze wykorzystywac innych metod niz const. getter do stored strring counter
+    private:
+        std::set<std::experimental::string_view, unsigned int> pool_; // here we store strings
+        size_t StoredStringCounter;
+    };
+
+}
+
+#endif //JIMP_EXERCISES_TEXTPOOL_H

c2dcd012fb304141e72286da9f68d916c7ed7855
Jakub Adamczyk <adamczyk777@gmail.com>
2017-04-08 19:56:57 +0200

    implement base logic for Word Class

--- lab6/wordcounter/Word.cpp
+++ lab6/wordcounter/Word.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "Word.h"
+//Constructors:
+Word::Word(const string &word) : word(word) {}
+
+Word::Word() {}
+
+//Accessors:
+const string &Word::getWord() const {
+    return word;
+}
+
+void Word::setWord(const string &word) {
+    Word::word = word;
+}
--- lab6/wordcounter/Word.h
+++ lab6/wordcounter/Word.h
@@ -5,9 +5,22 @@
 #ifndef JIMP_EXERCISES_WORD_H
 #define JIMP_EXERCISES_WORD_H
 
+#include <string>
+
+using std::string;
 
 class Word {
+private:
+    string word;
+public:
+    //Constructors:
+    Word(const string &word);
 
+    Word();
+    //Accesors:
+    const string &getWord() const;
+
+    void setWord(const string &word);
 };
 
 

139062e34ecf479eb70e0ca717ea8b29a4cd7018
Jakub Adamczyk <adamczyk777@gmail.com>
2017-04-08 19:53:54 +0200

    implement base Counts Logic

--- lab6/wordcounter/Counts.cpp
+++ lab6/wordcounter/Counts.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "Counts.h"
+
+//CONSTRUCTORS:
+Counts::Counts(int wordCount) : wordCount(wordCount) {
+    this->wordCount = wordCount;
+}
+
+Counts::Counts() {
+    this->wordCount = 0;
+}
+
+//ACCESSORS:
+int Counts::getWordCount() const {
+    return wordCount;
+}
+
+void Counts::setWordCount(int wordCount) {
+    Counts::wordCount = wordCount;
+}
+
+void Counts::addOccurrence() {
+    this->wordCount++;
+}
+
--- lab6/wordcounter/Counts.h
+++ lab6/wordcounter/Counts.h
@@ -7,7 +7,20 @@
 
 
 class Counts {
+private:
+    int wordCount;
+public:
+    //CONSTRUCTORS:
+    Counts(int wordCount);
 
+    Counts();
+
+    // ACCESSORS:
+    int getWordCount() const;
+
+    void setWordCount(int wordCount);
+
+    void addOccurrence();
 };
 
 

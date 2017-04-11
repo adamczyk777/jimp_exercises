072f28159535d950c74b87fdc2410ce99cd9bf9d
Krzysiek Kosecki <junnex.tay@gmail.com>
2017-04-12 01:19:18 +0200

    WordCounter some work

--- lab6/wordcounter/WordCounter.cpp
+++ lab6/wordcounter/WordCounter.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "WordCounter.h"
+
+void WordCounter::FromInputStream(std::string *istream) {
+
+}
+
+WordCounter::WordCounter() {
+
+}
+
+WordCounter::WordCounter(const std::initializer_list<std::vector<std::string>> &words) {
+
+    //
+}
--- lab6/wordcounter/WordCounter.h
+++ lab6/wordcounter/WordCounter.h
@@ -5,7 +5,23 @@
 #ifndef JIMP_EXERCISES_WORDCOUNTER_H
 #define JIMP_EXERCISES_WORDCOUNTER_H
 
+#include <string>
+#include <vector>
+#include "Word.h"
+#include "Counts.h"
+
 class WordCounter {
+
+    static void FromInputStream(std::string *istream);
+
+public:
+    WordCounter();
+
+    WordCounter(const std::initializer_list<std::vector<std::string>> &words);
+private:
+    // int wordsCount;
+    // std::vector<std::string> words;
+    std::vector<std::pair<Word,Counts>> words; // obiekty z jednym polem
 
 };
 

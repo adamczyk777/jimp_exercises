ea142a5c9b31154f74b61e8182eb839eb6ab9182
Jakub Adamczyk <adamczyk777@gmail.com>
2017-04-15 19:17:10 +0200

    add comments and couts

--- lab7/punkt3d/Point3D.cpp
+++ lab7/punkt3d/Point3D.cpp
@@ -7,18 +7,29 @@
 #include <ostream>
 #include <iostream>
 
-Point3D::Point3D() {}
+using ::std::ostream;
+using ::std::endl;
+using ::std::pow;
+using ::std::sqrt;
+using ::std::cout;
+
+Point3D::Point3D() {
+    cout << "Konstruktor Bezparametrowy punktu3d" << endl;
+
+}
 
 Point3D::Point3D(double x, double y, double z) : Point(x, y) {
     this->z_ = z;
+    cout << "Konstruktor Punktu3D, uzywa konstruktora zwyklego punktu" << endl;
 }
 
 Point3D::~Point3D() : ~Point() {
+    cout << "Destruktor P3D! Nic nie robie, bo nie musze zwalniać pamięci!" << endl;
 
 }
 
 double Point3D::Distance(const Point3D &other) const {
-    return sqrt(pow(GetZ() - other.GetZ(), 2) + pow(GetX()-other.GetX(),2)+pow(GetY()-other.GetY(),2));
+    return sqrt(pow(GetZ() - other.GetZ(), 2) + pow(GetX() - other.GetX(), 2) + pow(GetY() - other.GetY(), 2));
 }
 
 double Point3D::GetZ() const {

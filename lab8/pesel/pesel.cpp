0b7d658dc13cd77388a145f4a19af47c1b5bc2ef
tay0108 <kjkosecki@gmail.com>
2017-04-25 11:45:31 +0200

    lab8 pesel

--- lab8/pesel/pesel.cpp
+++ lab8/pesel/pesel.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "pesel.h"
+
+pesel::pesel(std::string pesel) {
+    if(validatePesel) {
+        this->number = pesel;
+    }
+}
+
+bool pesel::validatePesel(std::string pesel) {
+        // dlugosc 11 znakow:
+        try {
+            pesel.size();
+        } catch()
+        // tylko cyfry
+        //
+        // suma kontrolna
+    return true;
--- lab8/pesel/pesel.h
+++ lab8/pesel/pesel.h
@@ -5,9 +5,16 @@
 #ifndef JIMP_EXERCISES_PESEL_H
 #define JIMP_EXERCISES_PESEL_H
 
+#include <string>
 
 class pesel {
+public:
+    pesel(const std::string pesel);
 
+    bool validatePesel(std::string);
+
+private:
+    std::string number;
 };
 
 

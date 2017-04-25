f2f60daa066546a203d462f2f5ce88dd90d22d8d
Jakub Adamczyk <adamczyk777@gmail.com>
2017-04-25 11:43:15 +0200

    first excercise done

--- lab8/exceptions_example/main.cpp
+++ lab8/exceptions_example/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
-class CircleException{
+class CircleException {
     // Oznacza ze nie mozna wyrysowac kola
 };
 
-class BallException : public CircleException{
+class BallException : public CircleException {
     // Oznacza ze nie mozna wyrysowac kuli
 };
 
@@ -13,13 +14,15 @@ void drawBall() {
     throw BallException();
 }
 
-int main(){
-    try{
+int main() {
+    try {
         drawBall();
-    }catch(CircleException a){
-        cout << "Blad podczas rysowania kola" << endl;
-    }catch(BallException b){
+    } catch (BallException b) {
         cout << "Blad podczas rysowania kuli" << endl;
+    } catch (CircleException a) {
+        cout << "Blad podczas rysowania kola" << endl;
     }
-
+/* Wystarczylo zamienic miejscami dwa catche poniewaz najpierw wywolywalismy klase ogolna ktora lapie takze szczegolna
+ * poniewaz szczegolna jest takze w istocie ta ogolna plus to co zadeklarowalismy dodatkowo, wiec najpierw trzeba
+ * sprawdzic podklase a dopiero potem ogolniejsza nadklase */
 }

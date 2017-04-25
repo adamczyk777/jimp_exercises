84267e4ec25772f71aacec0404aee9747d33c76b
tay0108 <kjkosecki@gmail.com>
2017-04-25 11:54:19 +0200

    lab8 pesel

--- lab8/pesel/pesel.cpp
+++ lab8/pesel/pesel.cpp
@@ -11,11 +11,17 @@ pesel::pesel(std::string pesel) {
 }
 
 bool pesel::validatePesel(std::string pesel) {
-        // dlugosc 11 znakow:
-        try {
-            pesel.size();
-        } catch()
-        // tylko cyfry
-        //
-        // suma kontrolna
+    // dlugosc 11 znakow:
+    if (pesel.size() != 11) {
+        throw(WrongSize(pesel.size()));
+    }
+    // tylko cyfry:
+    for(int i=0;i<pesel.size();i++) {
+        if(pesel[i] < 48 || pesel[i] > 57) {
+            throw(WrongCharacter(pesel[i]));
+        }
+    }
+    // suma kontrolna (9×a + 7×b + 3×c + 1×d + 9×e + 7×f + 3×g + 1×h + 9×i + 7×j):
+    for(int i=0;)
     return true;
+}

0bd0be3eb0b28c0a9d3bcb01efdd1529e3fb707b
Jakub Adamczyk <adamczyk777@gmail.com>
2017-05-09 11:49:41 +0200

    fix serializable

--- lab9/academiaserialzer/Serializable.h
+++ lab9/academiaserialzer/Serializable.h
@@ -5,11 +5,15 @@
 #ifndef JIMP_EXERCISES_SERIALIZABLE_H
 #define JIMP_EXERCISES_SERIALIZABLE_H
 
+namespace academia {
+    class Serializer {
 
-class Serializable {
-public:
-    virtual void Serialize(Serializer * serializer) = 0;
-};
+    };
 
+    class Serializable {
+    public:
+        virtual void Serialize(Serializer * serializer) = 0;
+    };
+}
 
 #endif //JIMP_EXERCISES_SERIALIZABLE_H

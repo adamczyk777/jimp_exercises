51faf0af804ec1a2a8c5965ea0eeacf15539de10
Jakub Adamczyk <adamczyk777@gmail.com>
2017-05-09 11:35:37 +0200

    add serialization.h

---
+++ lab9/academiaserialzer/Serialization.h
@@ -0,0 +1,12 @@
+//
+// Created by adamc on 09.05.2017.
+//
+
+#ifndef JIMP_EXERCISES_SERIALIZATION_H
+#define JIMP_EXERCISES_SERIALIZATION_H
+
+#include "Building.h"
+#include "Room.h"
+#include "Serializable.h"
+
+#endif //JIMP_EXERCISES_SERIALIZATION_H

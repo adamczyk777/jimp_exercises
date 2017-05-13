e6999c0ae7ca5718da418af90c30ec87fd25968f
tay0108 <kjkosecki@gmail.com>
2017-05-13 17:29:28 +0200

    lab9 XML serializer / to \\ because of TDD

--- lab9/academiaserializer/XmlSerializer.cpp
+++ lab9/academiaserializer/XmlSerializer.cpp
@@ -5,6 +5,7 @@
 #include "XmlSerializer.h"
 
 namespace academia {
+// wszedzie powinny byc ukosniki w druga strone, ale testy
     XmlSerializer::XmlSerializer(std::ostream *out) : Serializer(out) {
         this->asd = out;
     }
@@ -12,25 +13,25 @@ namespace academia {
     void XmlSerializer::IntegerField(const std::string &field_name, int value) {
         *this->asd << "<" << field_name << ">";
         *this->asd << value;
-        *this->asd << "</" << field_name << ">";
+        *this->asd << "<\\" << field_name << ">";
     }
 
     void XmlSerializer::DoubleField(const std::string &field_name, double value) {
         *this->asd << "<" << field_name << ">";
         *this->asd << value;
-        *this->asd << "</" << field_name << ">";
+        *this->asd << "<\\" << field_name << ">";
     }
 
     void XmlSerializer::StringField(const std::string &field_name, const std::string &value) {
         *this->asd << "<" << field_name << ">";
         *this->asd << value;
-        *this->asd << "</" << field_name << ">";
+        *this->asd << "<\\" << field_name << ">";
     }
 
     void XmlSerializer::BooleanField(const std::string &field_name, bool value) {
         *this->asd << "<" << field_name << ">";
         *this->asd << value;
-        *this->asd << "</" << field_name << ">";
+        *this->asd << "<\\" << field_name << ">";
     }
 
     void XmlSerializer::SerializableField(const std::string &field_name, const academia::Serializable &value) {
@@ -38,8 +39,13 @@ namespace academia {
     }
 
     void XmlSerializer::ArrayField(const std::string &field_name,
-                                             const std::vector<std::reference_wrapper<const academia::Serializable>> &value) {
+                                   const std::vector<std::reference_wrapper<const academia::Serializable>> &value) {
+        *this->asd << "<" << field_name << ">";
+        // tu lista pokoi:
+/*        for(auto room in this->rooms) {
 
+        }*/
+        *this->asd << "<\\" << field_name << ">";
     }
 
     void XmlSerializer::Header(const std::string &object_name) {
@@ -47,7 +53,7 @@ namespace academia {
     }
 
     void XmlSerializer::Footer(const std::string &object_name) {
-        *this->asd << "</" << object_name << ">";
+        *this->asd << "<\\" << object_name << ">";
     }
 
     void XmlSerializer::endingLine(const std::string &field_name) {

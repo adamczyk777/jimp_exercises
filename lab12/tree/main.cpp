103b0dfe404445698b36c7dac5b01ced8b9de405
Jakub Adamczyk <adamczyk777@gmail.com>
2017-05-30 20:19:35 +0200

    search working print working insert working :) all recursive

--- lab12/tree/main.cpp
+++ lab12/tree/main.cpp
@@ -6,27 +6,20 @@
 
 int main() {
 
-    node<int> a(5);
-    node<int> b(6);
-    node<int> c(11);
-    node<int> d(12);
-    node<int> l(9);
+    tree<int> tree1;
 
-    tree<int> tree1{&a};
+    tree1.insert(5);
+    tree1.insert(4);
+    tree1.insert(7);
+    tree1.insert(9);
+    tree1.insert(1);
+    tree1.insert(2);
 
-    tree1.Insert(&b);
-    tree1.Insert(&c);
-    tree1.Insert(&d);
-    tree1.Insert(&l);
-    cout << (a.getRight())->getValue() << endl;
-    cout << (b.getRight())->getValue() << endl;
-    cout << (c.getRight())->getValue() << endl;
+    tree1.printTree();
 
-    //expected nullptr
-    cout << tree1.Search(10)->getValue() << endl;
-
-    //expected value
-    cout << tree1.Search(6)->getValue() << endl;
+    cout << "That was a tree :)" << endl;
+    cout << tree1.search(5)->value << endl;
+    cout << tree1.search(0)->value << endl;
 
     return 0;
 }
--- lab12/tree/tree.h
+++ lab12/tree/tree.h
@@ -31,20 +31,55 @@ private:
     void insertHelper(node<T> *root, T value) {
         if (value < root->value) {
             if (!root->left) {
-                root->left->value = new node<T>(value);
+                root->left = new node<T>(value);
             } else {
-                insertHelper(root->left,  value);
+                insertHelper(root->left, value);
+            }
+        } else if (value >= root->value) {
+            if (!root->right) {
+                root->right = new node<T>(value);
+            } else {
+                insertHelper(root->right, value);
             }
         }
     }
 
+    node<T> *searchHelper(node<T> *root, T value) {
+        if (root->value == value) {
+            return root;
+        } else if (!root->left || !root->right) {
+            return nullptr;
+        } else if (root->value <= value) {
+            return searchHelper(root->right, value);
+        } else if (root->value > value) {
+            return searchHelper(root->left, value);
+        }
+    }
+
+    void printHelper(node<T> *root) {
+        if (!root) return;
+        printHelper(root->left);
+        cout << root->value << ' ';
+        printHelper(root->right);
+    }
+
 public:
+    tree() {}
+
+    void printTree() {
+        printHelper(root);
+    }
+
     void insert(T value) {
         if (root) {
             insertHelper(root, value);
         } else {
             this->root = new node<T>(value);
         }
+    }
+
+    node<T> *search(T value) {
+        return searchHelper(root, value);
     }
 
 };

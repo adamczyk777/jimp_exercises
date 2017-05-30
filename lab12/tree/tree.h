eb162e7d02e5a837056b8892d6de133f4ffea247
Jakub Adamczyk <adamczyk777@gmail.com>
2017-05-30 18:30:21 +0200

    refactor writing new code added node struct +_ started writing class for BST

--- lab12/tree/tree.h
+++ lab12/tree/tree.h
@@ -12,88 +12,88 @@ using std::endl;
 
 
 template<class T>
-class node {
-public:
-    node(T value) {
-        this->left = nullptr;
-        this->right = nullptr;
-        this->value = value;
-    }
-
-    node *getLeft() const {
-        return left;
-    }
-
-    void setLeft(node *left) {
-        node::left = left;
-    }
-
-    node *getRight() const {
-        return right;
-    }
-
-    void setRight(node *right) {
-        node::right = right;
-    }
-
-    T getValue() const {
-        return value;
-    }
-
-    void setValue(T value) {
-        node::value = value;
-    }
-
-private:
+struct node {
+    T value;
     node *left;
     node *right;
-    T value;
+
+    node(T value) : value(value) {}
+
+    node(T value, node *left, node *right) : value(value), left(left), right(right) {}
+
 };
 
 template<class T>
 class tree {
-public:
-    tree(node<T> *root) : root(root) {}
-
-    node<T> *Insert(node<T> *element) {
-        node<T> *tmp = root;
-        while (tmp != nullptr) {
-            if (element->getValue() >= tmp->getValue()) {
-                if (tmp->getRight() != nullptr) {
-                    tmp = (tmp->getRight());
-                } else {
-                    tmp->setRight(element);
-                    return element;
-                }
-            } else {
-                if (tmp->getLeft() != nullptr) {
-                    tmp = (tmp->getLeft());
-                } else {
-                    tmp->setLeft(element);
-                    return element;
-                }
-            }
-        }
-    }
-
-    node<T> *Search(T value) {
-        node<T> *tmp = root;
-        while (tmp != nullptr) {
-            if (tmp->getValue() == value) {
-                return tmp;
-            }
-            if (tmp->getValue() < value) {
-                tmp = tmp->getRight();
-            } else {
-                tmp = tmp->getLeft();
-            }
-        }
-        return nullptr;
-    }
-
 private:
     node<T> *root;
+
+    void insertHelper(node<T> *root, T value) {
+        if (value < root->value) {
+            if (!root->left) {
+                root->left->value = new node<T>(value);
+            } else {
+                insertHelper(root->left,  value);
+            }
+        }
+    }
+
+public:
+    void insert(T value) {
+        if (root) {
+            insertHelper(root, value);
+        } else {
+            this->root = new node<T>(value);
+        }
+    }
+
 };
+
+// Legacy Code:
+//template<class T>
+//class tree {
+//public:
+//    tree(node<T> *root) : root(root) {}
+//
+//    node<T> *Insert(node<T> *element) {
+//        node<T> *tmp = root;
+//        while (tmp != nullptr) {
+//            if (element->getValue() >= tmp->getValue()) {
+//                if (tmp->getRight() != nullptr) {
+//                    tmp = (tmp->getRight());
+//                } else {
+//                    tmp->setRight(element);
+//                    return element;
+//                }
+//            } else {
+//                if (tmp->getLeft() != nullptr) {
+//                    tmp = (tmp->getLeft());
+//                } else {
+//                    tmp->setLeft(element);
+//                    return element;
+//                }
+//            }
+//        }
+//    }
+//
+//    node<T> *Search(T value) {
+//        node<T> *tmp = root;
+//        while (tmp != nullptr) {
+//            if (tmp->getValue() == value) {
+//                return tmp;
+//            }
+//            if (tmp->getValue() < value) {
+//                tmp = tmp->getRight();
+//            } else {
+//                tmp = tmp->getLeft();
+//            }
+//        }
+//        return nullptr;
+//    }
+//
+//private:
+//    node<T> *root;
+//};
 
 
 #endif //JIMP_EXERCISES_TREE_H

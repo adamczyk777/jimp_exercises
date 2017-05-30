4e324334a8f07da10ebaa4bcdcba12ac90438380
Jakub Adamczyk <adamczyk777@gmail.com>
2017-05-30 12:12:06 +0200

    add more tests

--- lab12/tree/main.cpp
+++ lab12/tree/main.cpp
@@ -8,12 +8,25 @@ int main() {
 
     node<int> a(5);
     node<int> b(6);
+    node<int> c(11);
+    node<int> d(12);
+    node<int> l(9);
 
     tree<int> tree1{&a};
-     
-    tree1.Insert(&b);
-    cout << a.getRight()->getValue() << endl;
 
+    tree1.Insert(&b);
+    tree1.Insert(&c);
+    tree1.Insert(&d);
+    tree1.Insert(&l);
+    cout << (a.getRight())->getValue() << endl;
+    cout << (b.getRight())->getValue() << endl;
+    cout << (c.getRight())->getValue() << endl;
+
+    //expected nullptr
+    cout << tree1.Search(10) << endl;
+
+    //expected value
+    cout << tree1.Search(6) << endl;
 
     return 0;
 }

d2c4a5524d0d430e138b803f89dfcf5f1684b4ab
Jakub Adamczyk <adamczyk777@gmail.com>
2017-06-06 12:20:16 +0200

    added tests for toRoman

--- lab13/arabicroman/ArabicRoman.cpp
+++ lab13/arabicroman/ArabicRoman.cpp
@@ -5,10 +5,11 @@
 #include "ArabicRoman.h"
 #include <map>
 #include<iostream>
+#include <sstream>
 
 int ArabicRoman::ToArabic(std::string roman) {
     int arabic = 0;
-    std::map roman_numerals<std::string, int>;
+    std::map<std::string, int> roman_numerals;
     roman_numerals["M"] = 1000;
     roman_numerals["CM"] = 900;
     roman_numerals["D"] = 500;
@@ -23,20 +24,73 @@ int ArabicRoman::ToArabic(std::string roman) {
     roman_numerals["IV"] = 4;
     roman_numerals["I"] = 1;
 
-    std::string current = roman.substr(0,2);
+    std::string current = roman.substr(0, 2);
 
-    while(current[1] != '\0') {
-            if(roman_numerals.count(current)) {
-                std::cout << "test";
+    while (current[1] != '\0') {
+        if (roman_numerals.count(current)) {
+            std::cout << "test";
         }
     }
     return arabic;
 }
 
 string ArabicRoman::toRoman(int arabic) {
-    string roman;
+    std::stringstream roman;
 
+    if (arabic < 1 || arabic > 3999)
+        return "Invalid Roman Number Value";
 
+    while (arabic >= 1000) {
+        roman << "M";
+        arabic -= 1000;        }
+    while (arabic >= 900) {
+        roman << "CM";
+        arabic -= 900;
+    }
+    while (arabic >= 500) {
+        roman << "D";
+        arabic -= 500;
+    }
+    while (arabic >= 400) {
+        roman << "CD";
+        arabic -= 400;
+    }
+    while (arabic >= 100) {
+        roman<< "C";
+        arabic -= 100;
+    }
+    while (arabic >= 90) {
+        roman << "XC";
+        arabic -= 90;
+    }
+    while (arabic >= 50) {
+        roman << "L";
+        arabic -= 50;
+    }
+    while (arabic >= 40) {
+        roman << "XL";
+        arabic -= 40;
+    }
+    while (arabic >= 10) {
+        roman << "X";
+        arabic -= 10;
+    }
+    while (arabic >= 9) {
+        roman << "IX";
+        arabic -= 9;
+    }
+    while (arabic >= 5) {
+        roman << "V";
+        arabic -= 5;
+    }
+    while (arabic >= 4) {
+        roman << "IV";
+        arabic -= 4;
+    }
+    while (arabic >= 1) {
+        roman << "I";
+        arabic -= 1;
+    }
 
-    return roman;
+    return roman.str();
 }
--- tests/lab13test/ArabicToRomanTest.cpp
+++ tests/lab13test/ArabicToRomanTest.cpp
@@ -4,10 +4,19 @@
 #include <gtest/gtest.h>
 #include "ArabicRoman.h"
 
-TEST(arabicToRomanTest, returnsCorrectInteger){
+TEST(arabicToRomanTest, returnsCorrectInteger) {
     EXPECT_EQ("X", ArabicRoman::toRoman(10));
     EXPECT_EQ("IX", ArabicRoman::toRoman(9));
-    EXPECT_EQ("ID", ArabicRoman::toRoman(499));
-    EXPECT_EQ("LDVII", ArabicRoman::toRoman(457));
+    EXPECT_EQ("CDXCIX", ArabicRoman::toRoman(499));
+    EXPECT_EQ("CDLVII", ArabicRoman::toRoman(457));
     EXPECT_EQ("MCMXCVII", ArabicRoman::toRoman(1997));
+}
+
+TEST(arabicToRomanTest, returnsInvalidInteger) {
+    EXPECT_NE("", ArabicRoman::toRoman(12));
+}
+
+TEST(arabicToRomanTest, returnsInvalidFormatString) { // TODO: DO EXCEPTIONS
+    EXPECT_EQ("Invalid Roman Number Value", ArabicRoman::toRoman(-10));
+    EXPECT_EQ("Invalid Roman Number Value", ArabicRoman::toRoman(9999999));
 }

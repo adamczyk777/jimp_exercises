38010dcb17be004122e1ebd26be1c57ec83b37ae
Jakub Adamczyk <adamczyk777@gmail.com>
2017-06-06 12:28:26 +0200

    added tests for ToArabic

--- tests/lab13test/RomanToArabicTest.cpp
+++ tests/lab13test/RomanToArabicTest.cpp
@@ -1,5 +1,14 @@
 //
 // Created by mwypych on 01.06.17.
 //
+#include <gtest/gtest.h>
+#include "ArabicRoman.h"
 
-TEST()
+TEST(romanToArabicTest, stringReturnedIsValidRomanNumeral) {
+    EXPECT_EQ(1508, ArabicRoman::ToArabic("MDVIII"));
+    EXPECT_EQ(5, ArabicRoman::ToArabic("V"));
+    EXPECT_EQ(10, ArabicRoman::ToArabic("X"));
+    EXPECT_EQ(1900, ArabicRoman::ToArabic("MCM"));
+    EXPECT_EQ(99, ArabicRoman::ToArabic("XCIX"));
+
+}

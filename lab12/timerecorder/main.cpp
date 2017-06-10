5fb7c2a938942ae42ca44a3cfc0feda8f3e7a92e
Krzysiek Kosecki <junnex.tay@gmail.com>
2017-06-10 22:17:43 +0200

    Lab12 homework setup

---
+++ lab12/timerecorder/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Tay on 6/10/2017.
+//
+
+#include <chrono>
+#include <utility>
+
+template <typename T>
+std::pair<double,T> timeRecoder() { // I guess time is in double, gonna check it
+    auto startTime = std::chrono::;
+    wykonaj ten obiekt czy cos
+    auto endTime = std::chrono::zmierz czas koncowy
+    odejmij
+    return ;
+}
+
+
+int main() {
+    return 0;
+}

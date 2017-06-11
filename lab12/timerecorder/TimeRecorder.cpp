29333c57d2bbb0e209224457791345a21ff09239
Jakub Adamczyk <adamczyk777@gmail.com>
2017-06-11 10:40:39 +0200

    fix CMakeError

---
+++ lab12/timerecorder/TimeRecorder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jadamczyk on 11.06.17.
+//
+
+#include "TimeRecorder.h"
---
+++ lab12/timerecorder/TimeRecorder.h
@@ -0,0 +1,14 @@
+//
+// Created by jadamczyk on 11.06.17.
+//
+
+#ifndef JIMP_EXERCISES_TIMERECORDER_H
+#define JIMP_EXERCISES_TIMERECORDER_H
+
+
+class TimeRecorder {
+
+};
+
+
+#endif //JIMP_EXERCISES_TIMERECORDER_H
